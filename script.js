83e2ea24d7591a563857cdb08807e62f53d794d2
Tegah Tiana <tegahtiana14@gmail.com>
2026-02-10 09:46:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
         // Theme Toggle
-        const themeToggle = document.getElementById('themeToggle');
-        const themeIcon = themeToggle.querySelector('i');
+        // const themeToggle = document.getElementById('themeToggle');
+        // const themeIcon = themeToggle.querySelector('i');
         
-        themeToggle.addEventListener('click', () => {
-            document.documentElement.setAttribute('data-theme', 
-                document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light'
-            );
+        // themeToggle.addEventListener('click', () => {
+        //     document.documentElement.setAttribute('data-theme', 
+        //         document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light'
+        //     );
             
-            if (document.documentElement.getAttribute('data-theme') === 'dark') {
-                themeIcon.classList.remove('fa-moon');
-                themeIcon.classList.add('fa-sun');
-            } else {
-                themeIcon.classList.remove('fa-sun');
-                themeIcon.classList.add('fa-moon');
-            }
-        });
+        //     if (document.documentElement.getAttribute('data-theme') === 'dark') {
+        //         themeIcon.classList.remove('fa-moon');
+        //         themeIcon.classList.add('fa-sun');
+        //     } else {
+        //         themeIcon.classList.remove('fa-sun');
+        //         themeIcon.classList.add('fa-moon');
+        //     }
+        // });
         
         // Smooth scrolling for anchor links
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
